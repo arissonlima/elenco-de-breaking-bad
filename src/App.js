22fd1fd1d036a19814b776287bf41e3ae68ce7af
Arisson Lima <arissonlima@gmail.com>
2022-03-29 22:52:36 -0300

    App.js logic and created components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from "axios";
+
+import Header from "./components/ui/Header";
+import CharacterGrid from "./components/characters/CharacterGrid";
 
 function App() {
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const result = await axios(
+        `https://www.breakingbadapi.com/api/characters`
+      );
+
+      console.log(result.data);
+      setItems(result.data);
+      setIsLoading(false);
+    };
+
+    fetchItems();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="container">
-        <h1>Hello World!</h1>
-      </header>
+    <div className="container">
+      <Header />
+      <CharacterGrid isLoading={isLoading} items={items} />
     </div>
   );
 }
